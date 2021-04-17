b7ecb41ea4d225e9b5a5d2dee209924094ae5b2d
koushik <koushik.koushik.kumar@gmail.com>
2021-04-17 20:55:32 +0530

    first API for Dashboard IMplemented, sends basic user Details

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -3,13 +3,13 @@ const router = require("express").Router();
 const pool = require("../db");
 const authorization = require("../middleware/authorization");
 
-router.get("/", authorization, async (req, res) => {
+router.post("/", authorization, async (req, res) => {
   try {
     const user = await pool.query(
       "Select userid,username,user_role from users where userid = $1",
       [req.user]
     );
-    res.json(user.rows[0]);
+    res.status(200).json(user.rows[0]);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Server Error");
